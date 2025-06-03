79610b6a01c1a8e4ea319dd1ac975d977a0c592e
seminify <seminify@gmail.com>
2025-06-03 11:08:31 +0900

    test

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
 
-const router = createBrowserRouter([
-  { path: "/", Component: App },
-  { path: "/app", Component: App },
-]);
+const router = createBrowserRouter(
+  [
+    { path: "/", Component: App },
+    { path: "/app", Component: App },
+  ],
+  {
+    basename: "/test",
+  }
+);
 
 export default router;
